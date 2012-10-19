957d191bc3ebaa07c6e07ef761b7c627cdacc3f3
Josh Schueths <josh.schueths@gmail.com>
2012-10-18 19:16:43 -0500

    Solution to rna transcription complete

---
+++ rna_transcription/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+void usage(string argv0)
+{
+	cout << "usage: argv0 <input file>" << endl;
+	return;
+}
+
+int main(int argc, char * argv[])
+{
+	if(argc < 2)
+	{
+		string call = argv[0];
+		usage(call);
+		exit(1);
+	}
+	
+	string sequence;
+	ifstream inFile;
+	inFile.open(argv[1]);
+	inFile >> sequence;
+	unsigned int size = sequence.size();
+	for(unsigned int i = 0; i < size; i++)
+	{
+		if(sequence[i] == 'T')
+			sequence[i] = 'U';
+	}
+	cout << sequence << endl;
+	return 0;
+}
+
