ab4c25528ecd329de4276b161a32e5ad8b9d43d2
Josh Schueths <josh.schueths@gmail.com>
2021-03-27 23:26:31 -0500

    Updates and update minimum required CMake

--- src/rna/rna_transcription.cpp
+++ src/rna/rna_transcription.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -7,26 +8,22 @@
 
 std::string process(const std::string &inputFile)
 {
-	std::string sequence;
-	std::ifstream inFile;
-	inFile.open(inputFile.c_str());
-	inFile >> sequence;
-	for(size_t i = 0; i < sequence.size(); i++)
-	{
-		if(sequence[i] == 'T')
-			sequence[i] = 'U';
-	}
-	return sequence;
+    std::string sequence;
+    std::ifstream inFile;
+    inFile.open(inputFile.c_str());
+    inFile >> sequence;
+    std::replace(std::begin(sequence), std::end(sequence), 'T', 'U');
+    return sequence;
 }
 
 int main(int argc, char * argv[])
 {
-	std::string inputFile;
-	boost::program_options::options_description desc;
-	desc.add_options()
-			(",i", boost::program_options::value<std::string>(&inputFile)->required(), "Input file");
-	processArgs(argc, argv, desc);
-	std::cout << process(inputFile) << std::endl;
-	return 0;
+    std::string inputFile;
+    boost::program_options::options_description desc;
+    desc.add_options()
+            (",i", boost::program_options::value<std::string>(&inputFile)->required(), "Input file");
+    processArgs(argc, argv, desc);
+    std::cout << process(inputFile) << std::endl;
+    return 0;
 }
 
