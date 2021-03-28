31a3ec354d3ba805876b163cacf161933238a465
Josh Schueths <josh.schueths@gmail.com>
2021-03-27 23:26:26 -0500

    Formatting updates
    More formatting updates

--- src/fib/fibonacci_rabbits.cpp
+++ src/fib/fibonacci_rabbits.cpp
@@ -3,57 +3,50 @@
 #include <boost/cstdint.hpp>
 #include <boost/program_options.hpp>
 
-struct Args
-{
-		boost::uint32_t n;
-		boost::uint32_t k;
+struct Args {
+    int n;
+    int k;
 };
 
-bool parseArgs(int argc, char* argv[], Args &args)
-{
-	namespace po = boost::program_options;
-	po::options_description desc("Options");
-	desc.add_options()
-			(",n", po::value<boost::uint32_t>(&args.n)->required(), "Number of months")
-			(",k", po::value<boost::uint32_t>(&args.k)->required(), "Pairs per generation");
-	po::variables_map vm;
-	try
-	{
-		po::store(po::parse_command_line(argc, argv, desc), vm);
-		po::notify(vm);
-	}
-	catch(po::error &e)
-	{
-		std::cerr << "ERROR: " << e.what() << std::endl;
-		std::cerr << desc << std::endl;
-		return false;
-	}
-	return true;
+Args parseArgs(int argc, char* argv[]) {
+    Args args = {0, 0};
+    namespace po = boost::program_options;
+    po::options_description desc("Options");
+    desc.add_options()
+            (",n", po::value<int>(&args.n)->required(), "Number of months")
+            (",k", po::value<int>(&args.k)->required(), "Pairs per generation");
+    po::variables_map vm;
+    try {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    }
+    catch(po::error &e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cerr << desc << std::endl;
+    }
+    return args;
 }
 
-boost::uint64_t processRabbits(boost::uint32_t months, boost::uint32_t rate)
-{
-	boost::uint64_t mature = 0;
-	boost::uint64_t young = 1;
-	boost::uint64_t result;
-	boost::uint64_t nextMature;
-	boost::uint64_t nextYoung;
-	for(size_t i = 0; i < months; i++)
-	{
-		result = mature + young;
-		nextMature = mature + young;
-		nextYoung = mature * rate;
-		mature = nextMature;
-		young = nextYoung;
-	}
-	return result;
+int processRabbits(int months, int rate) {
+    int mature = 0;
+    int young = 1;
+    int result = 0;
+    int nextMature = 0;
+    int nextYoung = 0;
+    for(int i = 0; i < months; i++)
+    {
+        result = mature + young;
+        nextMature = mature + young;
+        nextYoung = mature * rate;
+        mature = nextMature;
+        young = nextYoung;
+    }
+    return result;
 }
 
-int main(int argc, char* argv[])
-{
-	Args args;
-	parseArgs(argc, argv, args);
-	boost::uint32_t numPairs = processRabbits(args.n, args.k);
-	std::cout << numPairs << std::endl;
-	return 0;
+int main(int argc, char* argv[]) {
+    Args args = parseArgs(argc, argv);
+    int numPairs = processRabbits(args.n, args.k);
+    std::cout << numPairs << std::endl;
+    return 0;
 }
